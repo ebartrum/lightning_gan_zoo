2b19585c3a3c5a3f0a3b06e4c71a8c659e4e09bb
Ed Bartrum <edward.bartrum@gmail.com>
2021-01-15 11:54:10 +0000

    Uncomment Batchnorm

--- core/networks.py
+++ core/networks.py
@@ -34,7 +34,7 @@ class Discriminator(nn.Module):
                 padding,
                 bias=False,
             ),
-            #nn.BatchNorm2d(out_channels),
+            nn.BatchNorm2d(out_channels),
             nn.LeakyReLU(0.2),
         )
 
@@ -68,7 +68,7 @@ class Generator(nn.Module):
                 padding,
                 bias=False,
             ),
-            #nn.BatchNorm2d(out_channels),
+            nn.BatchNorm2d(out_channels),
             nn.ReLU(),
         )
 
