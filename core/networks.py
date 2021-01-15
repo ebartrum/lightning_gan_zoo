1d681697b401ba1e5d909535f57653d61b848928
Ed Bartrum <edward.bartrum@gmail.com>
2021-01-15 13:00:14 +0000

    Name the generator blocks

--- core/networks.py
+++ core/networks.py
@@ -43,18 +43,18 @@ class Discriminator(nn.Module):
 class Generator(nn.Module):
     def __init__(self, channels_noise, channels_img, features_g):
         super(Generator, self).__init__()
-        self.net = nn.Sequential(
+        self.net = nn.Sequential(OrderedDict([
             # Input: N x channels_noise x 1 x 1
-            self._block(channels_noise, features_g * 16, 4, 1, 0),  # img: 4x4
-            self._block(features_g * 16, features_g * 8, 4, 2, 1),  # img: 8x8
-            self._block(features_g * 8, features_g * 4, 4, 2, 1),  # img: 16x16
-            self._block(features_g * 4, features_g * 2, 4, 2, 1),  # img: 32x32
-            nn.ConvTranspose2d(
-                features_g * 2, channels_img, kernel_size=4, stride=2, padding=1
-            ),
+            ('block1', self._block(channels_noise, features_g * 16, 4, 1, 0)),  # img: 4x4
+            ('block2', self._block(features_g * 16, features_g * 8, 4, 2, 1)),  # img: 8x8
+            ('block3', self._block(features_g * 8, features_g * 4, 4, 2, 1)),  # img: 16x16
+            ('block4', self._block(features_g * 4, features_g * 2, 4, 2, 1)),  # img: 32x32
+            ('transpose_conv_out', nn.ConvTranspose2d(
+                features_g * 2, channels_img, kernel_size=4,
+                stride=2, padding=1)),
             # Output: N x channels_img x 64 x 64
-            nn.Tanh(),
-        )
+            ('tanh', nn.Tanh()),
+            ]))
 
     def _block(self, in_channels, out_channels, kernel_size, stride, padding):
         return nn.Sequential(
