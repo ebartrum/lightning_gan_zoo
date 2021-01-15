d0ec14e8cc60da7db50ea8e713b7b383c1d2a315
Ed Bartrum <edward.bartrum@gmail.com>
2021-01-15 12:58:08 +0000

    Name the discriminator layers

--- core/networks.py
+++ core/networks.py
@@ -3,38 +3,39 @@ Discriminator and Generator implementation from DCGAN paper
 """
 import torch
 import torch.nn as nn
+from collections import OrderedDict
 
 class Discriminator(nn.Module):
     def __init__(self, channels_img, features_d):
         super(Discriminator, self).__init__()
-        self.disc = nn.Sequential(
+        self.disc = nn.Sequential(OrderedDict([
             # input: N x channels_img x 64 x 64
-            nn.Conv2d(
+            ('conv_in', nn.Conv2d(
                 channels_img, features_d, kernel_size=4, stride=2, padding=1
-            ),
-            nn.LeakyReLU(0.2),
+            )),
+            ('leaky_relu', nn.LeakyReLU(0.2)),
             # _block(in_channels, out_channels, kernel_size, stride, padding)
-            self._block(features_d, features_d * 2, 4, 2, 1),
-            self._block(features_d * 2, features_d * 4, 4, 2, 1),
-            self._block(features_d * 4, features_d * 8, 4, 2, 1),
+            ('block1', self._block(features_d, features_d * 2, 4, 2, 1)),
+            ('block2', self._block(features_d * 2, features_d * 4, 4, 2, 1)),
+            ('block3', self._block(features_d * 4, features_d * 8, 4, 2, 1)),
             # After all _block img output is 4x4 (Conv2d below makes into 1x1)
-            nn.Conv2d(features_d * 8, 1, kernel_size=4, stride=2, padding=0),
-            nn.Sigmoid(),
-        )
+            ('conv_out', nn.Conv2d(features_d * 8, 1, kernel_size=4, stride=2, padding=0)),
+            ('sigmoid', nn.Sigmoid()),
+            ]))
 
     def _block(self, in_channels, out_channels, kernel_size, stride, padding):
-        return nn.Sequential(
-            nn.Conv2d(
+        return nn.Sequential(OrderedDict([
+            ('conv', nn.Conv2d(
                 in_channels,
                 out_channels,
                 kernel_size,
                 stride,
                 padding,
                 bias=False,
-            ),
-            nn.BatchNorm2d(out_channels),
-            nn.LeakyReLU(0.2),
-        )
+                )),
+            ('batch_norm', nn.BatchNorm2d(out_channels)),
+            ('leaky_relu', nn.LeakyReLU(0.2)),
+            ]))
 
     def forward(self, x):
         return self.disc(x)
