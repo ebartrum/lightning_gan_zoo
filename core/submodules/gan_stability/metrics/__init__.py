e41ceb4a2381d5cc407e1778e01ae6307355dcc5
Ed Bartrum <edward.bartrum@gmail.com>
2021-01-18 14:27:00 +0000

    Submodules required for fid/kid

---
+++ core/submodules/gan_stability/metrics/__init__.py
@@ -0,0 +1,9 @@
+from .inception_score import inception_score
+from .fid_score import FIDEvaluator
+from .kid_score import KIDEvaluator
+
+__all__ = [
+    inception_score,
+    FIDEvaluator,
+    KIDEvaluator
+]
