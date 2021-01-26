509d9392a6e92eb3b0bd9b2ce0fd51cf976e8d09
Ed Bartrum <edward.bartrum@gmail.com>
2021-01-26 17:19:48 +0000

    wgan_gp initial commit

--- core/training_steps.py
+++ core/training_steps.py
@@ -48,3 +48,25 @@ def wgan(lm, batch, batch_idx, optimizer_idx):
         loss_gen = -torch.mean(gen_fake)
         lm.log('train/g_loss', loss_gen)
         return loss_gen
+
+def wgan_gp(lm, batch, batch_idx, optimizer_idx):
+    import ipdb;ipdb.set_trace()
+    real, _ = batch
+    noise = torch.randn(lm.cfg.train.batch_size,
+            lm.cfg.train.noise_dim, 1, 1).to(lm.device)
+    fake = lm.generator(noise)
+
+    # train discriminator
+    if optimizer_idx == 0:
+        disc_real = lm.discriminator(real).reshape(-1)
+        disc_fake = lm.discriminator(fake.detach()).reshape(-1)
+        loss_disc = -(torch.mean(disc_real) - torch.mean(disc_fake))
+        lm.log('train/d_loss', loss_disc)
+        return loss_disc
+
+    # train generator
+    if optimizer_idx == 1:
+        gen_fake = lm.discriminator(fake).reshape(-1)
+        loss_gen = -torch.mean(gen_fake)
+        lm.log('train/g_loss', loss_gen)
+        return loss_gen
