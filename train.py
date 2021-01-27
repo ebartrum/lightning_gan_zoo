c63747865b8c740ae98e4015459f32bd1bfb0842
Ed Bartrum <edward.bartrum@gmail.com>
2021-01-27 18:39:21 +0000

    Add LR annealing option

--- train.py
+++ train.py
@@ -61,9 +61,15 @@ class GAN(pl.LightningModule):
                     self.discriminator.parameters())
         opt_gen = instantiate(self.cfg.optimiser,
                     self.generator.parameters())
-        return ({'optimizer': opt_disc,
+        scheduler_disc = optim.lr_scheduler.StepLR(opt_disc,
+                step_size=self.cfg.optimisation.anneal_every,
+                gamma=self.cfg.optimisation.lr_anneal)
+        scheduler_gen = optim.lr_scheduler.StepLR(opt_gen,
+                step_size=self.cfg.optimisation.anneal_every,
+                gamma=self.cfg.optimisation.lr_anneal)
+        return ({'optimizer': opt_disc, 'lr_scheduler': scheduler_disc,
                     'frequency': self.cfg.optimisation.disc_freq},
-               {'optimizer': opt_gen,
+               {'optimizer': opt_gen, 'lr_scheduler': scheduler_gen,
                    'frequency': self.cfg.optimisation.gen_freq})
 
     def train_dataloader(self):
