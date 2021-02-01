9edb3d628776a202d71cef3d6f1c6025723e95ec
Ed Bartrum <edward.bartrum@gmail.com>
2021-02-01 02:21:24 +0000

    Turn raysampler back nn.Module -> object

--- core/submodules/graf/graf/transforms.py
+++ core/submodules/graf/graf/transforms.py
@@ -1,25 +1,23 @@
 import torch
 from math import sqrt, exp
-from torch import nn
 from core.submodules.nerf_pytorch.run_nerf_helpers_mod import get_rays, get_rays_ortho
 
 
-class ImgToPatch(nn.Module):
+class ImgToPatch(object):
     def __init__(self, ray_sampler, hwf):
-        super(ImgToPatch, self).__init__()
         self.ray_sampler = ray_sampler
         self.hwf = hwf      # camera intrinsics
 
-    def forward(self, img):
+    def __call__(self, img):
         rgbs = []
         for img_i in img:
-            pose = torch.eye(4).to(img.device)         # use dummy pose to infer pixel values
+            pose = torch.eye(4).cuda()         # use dummy pose to infer pixel values
             _, selected_idcs, pixels_i = self.ray_sampler(H=self.hwf[0], W=self.hwf[1], focal=self.hwf[2], pose=pose)
             if selected_idcs is not None:
                 rgbs_i = img_i.flatten(1, 2).t()[selected_idcs]
             else:
                 rgbs_i = torch.nn.functional.grid_sample(img_i.unsqueeze(0), 
-                                     pixels_i.unsqueeze(0), mode='bilinear', align_corners=True)[0]
+                                     pixels_i.unsqueeze(0), mode='bilinear', align_corners=True)[0].cuda()
                 rgbs_i = rgbs_i.flatten(1, 2).t()
             rgbs.append(rgbs_i)
 
@@ -28,7 +26,7 @@ class ImgToPatch(nn.Module):
         return rgbs
 
 
-class RaySampler(nn.Module):
+class RaySampler(object):
     def __init__(self, N_samples, orthographic=False):
         super(RaySampler, self).__init__()
         self.N_samples = N_samples
@@ -36,14 +34,15 @@ class RaySampler(nn.Module):
         self.return_indices = True
         self.orthographic = orthographic
 
-    def forward(self, H, W, focal, pose):
+    def __call__(self, H, W, focal, pose):
         if self.orthographic:
             size_h, size_w = focal      # Hacky
             rays_o, rays_d = get_rays_ortho(H, W, pose, size_h, size_w)
         else:
             rays_o, rays_d = get_rays(H, W, focal, pose)
 
-        select_inds = self.sample_rays(H, W).to(pose.device)
+        rays_o, rays_d = rays_o.cuda(), rays_d.cuda()
+        select_inds = self.sample_rays(H, W)
 
         if self.return_indices:
             rays_o = rays_o.view(-1, 3)[select_inds]
@@ -52,13 +51,13 @@ class RaySampler(nn.Module):
             h = (select_inds // W) / float(H) - 0.5
             w = (select_inds %  W) / float(W) - 0.5
 
-            hw = torch.stack([h,w]).t()
+            hw = torch.stack([h,w]).t().cuda()
 
         else:
             rays_o = torch.nn.functional.grid_sample(rays_o.permute(2,0,1).unsqueeze(0), 
-                                 select_inds.unsqueeze(0), mode='bilinear', align_corners=True)[0]
+                                 select_inds.unsqueeze(0), mode='bilinear', align_corners=True)[0].cuda()
             rays_d = torch.nn.functional.grid_sample(rays_d.permute(2,0,1).unsqueeze(0), 
-                                 select_inds.unsqueeze(0), mode='bilinear', align_corners=True)[0]
+                                 select_inds.unsqueeze(0), mode='bilinear', align_corners=True)[0].cuda()
             rays_o = rays_o.permute(1,2,0).view(-1, 3)
             rays_d = rays_d.permute(1,2,0).view(-1, 3)
 
@@ -92,8 +91,8 @@ class FlexGridRaySampler(RaySampler):
         self.max_scale = max_scale
 
         # nn.functional.grid_sample grid value range in [-1,1]
-        self.w, self.h = torch.meshgrid([torch.linspace(-1,1,self.N_samples_sqrt),
-                                         torch.linspace(-1,1,self.N_samples_sqrt)])
+        self.w, self.h = torch.meshgrid([torch.linspace(-1,1,self.N_samples_sqrt).cuda(),
+                                         torch.linspace(-1,1,self.N_samples_sqrt).cuda()])
         self.h = self.h.unsqueeze(2)
         self.w = self.w.unsqueeze(2)
 
@@ -114,14 +113,14 @@ class FlexGridRaySampler(RaySampler):
 
         scale = 1
         if self.random_scale:
-            scale = torch.Tensor(1).uniform_(min_scale, self.max_scale)
+            scale = torch.Tensor(1).uniform_(min_scale, self.max_scale).cuda()
             h = self.h * scale 
             w = self.w * scale 
 
         if self.random_shift:
             max_offset = 1-scale.item()
-            h_offset = torch.Tensor(1).uniform_(0, max_offset) * (torch.randint(2,(1,)).float()-0.5)*2
-            w_offset = torch.Tensor(1).uniform_(0, max_offset) * (torch.randint(2,(1,)).float()-0.5)*2
+            h_offset = torch.Tensor(1).uniform_(0, max_offset).cuda() * (torch.randint(2,(1,)).cuda().float()-0.5)*2
+            w_offset = torch.Tensor(1).uniform_(0, max_offset).cuda() * (torch.randint(2,(1,)).cuda().float()-0.5)*2
 
             h += h_offset
             w += w_offset
