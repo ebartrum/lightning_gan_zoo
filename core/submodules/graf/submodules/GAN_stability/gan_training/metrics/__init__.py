9f22b7a7430aa7347da28c15ba3276dd346275da
Ed Bartrum <edward.bartrum@gmail.com>
2021-02-08 23:41:30 +0000

    Remove tracked graf

--- core/submodules/graf/submodules/GAN_stability/gan_training/metrics/__init__.py
+++
@@ -1,9 +0,0 @@
-from GAN_stability.gan_training.metrics.inception_score import inception_score
-from GAN_stability.gan_training.metrics.fid_score import FIDEvaluator
-from GAN_stability.gan_training.metrics.kid_score import KIDEvaluator
-
-__all__ = [
-    inception_score,
-    FIDEvaluator,
-    KIDEvaluator
-]
