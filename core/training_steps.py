6ee2ead56abfa4238f2ea6561fc3114bffb0d1b2
Ed Bartrum <edward.bartrum@gmail.com>
2021-03-30 20:09:28 +0100

    Specify training step in lightning module

--- core/training_steps.py
+++ core/training_steps.py
@@ -7,6 +7,7 @@ from torch.utils.data import DataLoader
 from core.utils import gradient_penalty, compute_grad2
 from core.utils import init_weights, VerboseShapeExecution
 from hydra.utils import instantiate, call
+from abc import abstractmethod
 
 class BaseGAN(pl.LightningModule):
     def __init__(self, cfg, logging_dir):
@@ -29,8 +30,9 @@ class BaseGAN(pl.LightningModule):
         if cfg.debug.verbose_shape:
             self.apply(VerboseShapeExecution)
 
+    @abstractmethod
     def training_step(self, batch, batch_idx, optimizer_idx):
-        return call(self.cfg.train.training_step, self, batch, batch_idx, optimizer_idx)
+        pass
 
     def validation_step(self, batch, batch_idx):
         real, _ = batch
@@ -78,100 +80,107 @@ class BaseGAN(pl.LightningModule):
         return DataLoader(dataset, num_workers=self.cfg.train.num_workers,
             batch_size=self.cfg.train.batch_size)
 
-def dc(lm, batch, batch_idx, optimizer_idx):
-    real, _ = batch
-    noise = torch.randn(len(real),
-            lm.cfg.train.noise_dim, 1, 1).to(lm.device)
-    fake = lm.generator(noise)
+class DCGAN(BaseGAN):
+    def training_step(self, batch, batch_idx, optimizer_idx):
+        real, _ = batch
+        noise = torch.randn(len(real),
+                self.cfg.train.noise_dim, 1, 1).to(self.device)
+        fake = self.generator(noise)
 
-    # train discriminator
-    if optimizer_idx == 0:
-        disc_real = lm.discriminator(real).reshape(-1)
-        loss_disc_real = lm.criterion(disc_real,
-                torch.ones_like(disc_real))
-        disc_fake = lm.discriminator(fake.detach()).reshape(-1)
-        loss_disc_fake = lm.criterion(disc_fake,
-                torch.zeros_like(disc_fake))
-        loss_disc = (loss_disc_real + loss_disc_fake) / 2
-        lm.log('train/d_loss', loss_disc)
-        return loss_disc
+        # train discriminator
+        if optimizer_idx == 0:
+            disc_real = self.discriminator(real).reshape(-1)
+            loss_disc_real = self.criterion(disc_real,
+                    torch.ones_like(disc_real))
+            disc_fake = self.discriminator(fake.detach()).reshape(-1)
+            loss_disc_fake = self.criterion(disc_fake,
+                    torch.zeros_like(disc_fake))
+            loss_disc = (loss_disc_real + loss_disc_fake) / 2
+            self.log('train/d_loss', loss_disc)
+            return loss_disc
 
-    # train generator
-    if optimizer_idx == 1:
-        output = lm.discriminator(fake).reshape(-1)
-        loss_gen = lm.criterion(output, torch.ones_like(output))
-        lm.log('train/g_loss', loss_gen)
-        return loss_gen
+        # train generator
+        if optimizer_idx == 1:
+            output = self.discriminator(fake).reshape(-1)
+            loss_gen = self.criterion(output, torch.ones_like(output))
+            self.log('train/g_loss', loss_gen)
+            return loss_gen
 
-def gan_stability_r1(lm, batch, batch_idx, optimizer_idx):
-    real, _ = batch
-    noise = torch.randn(len(real),
-            lm.cfg.train.noise_dim, 1, 1).to(lm.device)
-    fake = lm.generator(noise)
+class GANStabilityR1(BaseGAN):
+    def training_step(self, batch, batch_idx, optimizer_idx):
+        real, _ = batch
+        noise = torch.randn(len(real),
+                self.cfg.train.noise_dim, 1, 1).to(self.device)
+        fake = self.generator(noise)
 
-    # train discriminator
-    if optimizer_idx == 0:
-        real.requires_grad_()
-        disc_real = lm.discriminator(real).reshape(-1)
-        loss_disc_real = lm.criterion(disc_real,
-                torch.ones_like(disc_real))
-        disc_fake = lm.discriminator(fake.detach()).reshape(-1)
-        loss_disc_fake = lm.criterion(disc_fake,
-                torch.zeros_like(disc_fake))
-        r1_reg = lm.cfg.loss_weight.reg * compute_grad2(disc_real, real).mean()
-        loss_disc = r1_reg + (loss_disc_real + loss_disc_fake) 
-        lm.log('train/d_loss', loss_disc)
-        return loss_disc
+        # train discriminator
+        if optimizer_idx == 0:
+            real.requires_grad_()
+            disc_real = self.discriminator(real).reshape(-1)
+            loss_disc_real = self.criterion(disc_real,
+                    torch.ones_like(disc_real))
+            disc_fake = self.discriminator(fake.detach()).reshape(-1)
+            loss_disc_fake = self.criterion(disc_fake,
+                    torch.zeros_like(disc_fake))
+            r1_reg = self.cfg.loss_weight.reg * compute_grad2(disc_real, real).mean()
+            loss_disc = r1_reg + (loss_disc_real + loss_disc_fake) 
+            self.log('train/d_loss', loss_disc)
+            return loss_disc
 
-    # train generator
-    if optimizer_idx == 1:
-        output = lm.discriminator(fake).reshape(-1)
-        loss_gen = lm.criterion(output, torch.ones_like(output))
-        lm.log('train/g_loss', loss_gen)
-        return loss_gen
+        # train generator
+        if optimizer_idx == 1:
+            output = self.discriminator(fake).reshape(-1)
+            loss_gen = self.criterion(output, torch.ones_like(output))
+            self.log('train/g_loss', loss_gen)
+            return loss_gen
 
-def wgan(lm, batch, batch_idx, optimizer_idx):
-    for p in lm.discriminator.parameters(): #Clip discriminator weights
-        p.data.clamp_(-lm.cfg.train.weight_clip, lm.cfg.train.weight_clip)
+class WGAN(BaseGAN):
+    def training_step(self, batch, batch_idx, optimizer_idx):
+        for p in self.discriminator.parameters(): #Clip discriminator weights
+            p.data.clamp_(-self.cfg.train.weight_clip,
+                    self.cfg.train.weight_clip)
 
-    real, _ = batch
-    noise = torch.randn(len(real),
-            lm.cfg.train.noise_dim, 1, 1).to(lm.device)
-    fake = lm.generator(noise)
+        real, _ = batch
+        noise = torch.randn(len(real),
+                self.cfg.train.noise_dim, 1, 1).to(self.device)
+        fake = self.generator(noise)
 
-    # train discriminator
-    if optimizer_idx == 0:
-        disc_real = lm.discriminator(real).reshape(-1)
-        disc_fake = lm.discriminator(fake.detach()).reshape(-1)
-        loss_disc = -(torch.mean(disc_real) - torch.mean(disc_fake))
-        lm.log('train/d_loss', loss_disc)
-        return loss_disc
+        # train discriminator
+        if optimizer_idx == 0:
+            disc_real = self.discriminator(real).reshape(-1)
+            disc_fake = self.discriminator(fake.detach()).reshape(-1)
+            loss_disc = -(torch.mean(disc_real) - torch.mean(disc_fake))
+            self.log('train/d_loss', loss_disc)
+            return loss_disc
 
-    # train generator
-    if optimizer_idx == 1:
-        gen_fake = lm.discriminator(fake).reshape(-1)
-        loss_gen = -torch.mean(gen_fake)
-        lm.log('train/g_loss', loss_gen)
-        return loss_gen
+        # train generator
+        if optimizer_idx == 1:
+            gen_fake = self.discriminator(fake).reshape(-1)
+            loss_gen = -torch.mean(gen_fake)
+            self.log('train/g_loss', loss_gen)
+            return loss_gen
 
-def wgan_gp(lm, batch, batch_idx, optimizer_idx):
-    real, _ = batch
-    noise = torch.randn(len(real),
-            lm.cfg.train.noise_dim, 1, 1).to(lm.device)
-    fake = lm.generator(noise)
+class WGANGP(BaseGAN):
+    def training_step(self, batch, batch_idx, optimizer_idx):
+        real, _ = batch
+        noise = torch.randn(len(real),
+                self.cfg.train.noise_dim, 1, 1).to(self.device)
+        fake = self.generator(noise)
 
-    # train discriminator
-    if optimizer_idx == 0:
-        disc_real = lm.discriminator(real).reshape(-1)
-        disc_fake = lm.discriminator(fake.detach()).reshape(-1)
-        gp = gradient_penalty(lm.discriminator, real, fake, device=lm.device)
-        loss_disc = (lm.cfg.loss_weight.lambda_gp*gp)-(torch.mean(disc_real) - torch.mean(disc_fake))
-        lm.log('train/d_loss', loss_disc)
-        return loss_disc
+        # train discriminator
+        if optimizer_idx == 0:
+            disc_real = self.discriminator(real).reshape(-1)
+            disc_fake = self.discriminator(fake.detach()).reshape(-1)
+            gp = gradient_penalty(self.discriminator,
+                    real, fake, device=self.device)
+            loss_disc = (self.cfg.loss_weight.lambda_gp*gp)\
+                    -(torch.mean(disc_real) - torch.mean(disc_fake))
+            self.log('train/d_loss', loss_disc)
+            return loss_disc
 
-    # train generator
-    if optimizer_idx == 1:
-        gen_fake = lm.discriminator(fake).reshape(-1)
-        loss_gen = -torch.mean(gen_fake)
-        lm.log('train/g_loss', loss_gen)
-        return loss_gen
+        # train generator
+        if optimizer_idx == 1:
+            gen_fake = self.discriminator(fake).reshape(-1)
+            loss_gen = -torch.mean(gen_fake)
+            self.log('train/g_loss', loss_gen)
+            return loss_gen
