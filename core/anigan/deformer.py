03f90a3778d4813e14a5d7c759e7b40c9d0570de
Ed Bartrum <edward.bartrum@gmail.com>
2021-05-04 20:05:03 +0100

    Placeholder Kernel and RBF deformers

--- core/anigan/deformer.py
+++ core/anigan/deformer.py
@@ -19,3 +19,43 @@ class TPSDeformer(nn.Module):
         deformed_verts = deformed_verts[:,::self.template_subdivision]
         return tps_functions.transform(x, deformed_verts,
                 deformation_parameters)
+
+class KernelDeformer(nn.Module):
+    def __init__(self, template_subdivision, lambda_):
+        super(KernelDeformer, self).__init__()
+        self.template_subdivision = template_subdivision
+        self.lambda_ = lambda_
+        
+    def calculate_deformation(self, shape_analysis):
+        verts = shape_analysis['verts'][:,::self.template_subdivision]
+        template_verts =\
+            shape_analysis['mean_shape'][:,::self.template_subdivision]
+        coefficient = tps_functions.find_coefficients(
+            verts, template_verts, self.lambda_).detach()
+        return coefficient
+
+    def transform(self, x, deformed_verts, deformation_parameters):
+        import ipdb;ipdb.set_trace()
+        deformed_verts = deformed_verts[:,::self.template_subdivision]
+        return tps_functions.transform(x, deformed_verts,
+                deformation_parameters)
+
+class RBFDeformer(nn.Module):
+    def __init__(self, template_subdivision, lambda_):
+        super(KernelDeformer, self).__init__()
+        self.template_subdivision = template_subdivision
+        self.lambda_ = lambda_
+        
+    def calculate_deformation(self, shape_analysis):
+        verts = shape_analysis['verts'][:,::self.template_subdivision]
+        template_verts =\
+            shape_analysis['mean_shape'][:,::self.template_subdivision]
+        coefficient = tps_functions.find_coefficients(
+            verts, template_verts, self.lambda_).detach()
+        return coefficient
+
+    def transform(self, x, deformed_verts, deformation_parameters):
+        import ipdb;ipdb.set_trace()
+        deformed_verts = deformed_verts[:,::self.template_subdivision]
+        return tps_functions.transform(x, deformed_verts,
+                deformation_parameters)
